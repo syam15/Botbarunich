b0f8d76bd0083c2525d6ac4479bfa723dd5a4d8d
Lalu Muhammad Syamsul Hadi <33655622+syam15@users.noreply.github.com>
2025-02-25 21:24:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,11 @@
 global.owner = [
-  "6287765939693", //ganti nomor owner
-  "628" //nomor owner kedua kalo ada
-]
-let fs = require('fs')
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(`Update ${__filename}`)
-delete require.cache[file]
-require(file)
-})
-/*
-SCRIPT BY © VYNAA VALERIE 
-•• recode kasih credits 
-•• contacts: (t.me/VLShop2)
-•• instagram: @vynaa_valerie 
-•• (github.com/VynaaValerie) 
-*/
+    "6287765939693"
+  ]
+  let fs = require('fs')
+  let file = require.resolve(__filename)
+  fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(`Update ${__filename}`)
+  delete require.cache[file]
+  require(file)
+  })
